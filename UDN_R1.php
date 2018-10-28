f39e83e07bb03c5ebadb80f4702ba97dcc3f64a0
dennygod1220 <dennygod1220@gmail.com>
2018-10-28 20:02:52 +0800

    1028_read udn

--- UDN_R1.php
+++ UDN_R1.php
@@ -36,6 +36,8 @@ foreach ($dir as $val) {
 
 ///////////////////////////////xml 解析後 呼叫關鍵字API 將response存起來 //////////////////////////////////////////////////////////////////
 for($i=0;$i<count($decode["channel"]["item"]);$i++){
+// for($i=0; $i < 2; $i++){
+
     if (isset($decode["channel"]["item"][$i]["title"])) {
         $ch = curl_init();
         curl_setopt($ch,CURLOPT_URL,"http://35.234.18.81/result/udn?url=".$decode["channel"]["item"][$i]["link"]);
@@ -43,32 +45,45 @@ for($i=0;$i<count($decode["channel"]["item"]);$i++){
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 60); 
         $res = curl_exec($ch);
+        $res_ar = json_decode($res,true);
+        $res_ar["link"] =$decode["channel"]["item"][$i]["link"];
+        $res_ar["title"] = $decode["channel"]["item"][$i]["title"];
+        $res_js = json_encode($res_ar);
+        // var_dump(json_encode($res_ar));
+        // var_dump(json_encode($res_js));
+
         //取得http 狀態碼 
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         if (curl_errno($ch) == 28) {
             echo "Timeout";
         }
         curl_close($ch);
-        $title = $decode["channel"]["item"][$i]["title"];
+        // $title = $decode["channel"]["item"][$i]["title"];
         
         //要建立的檔案
-        $TxtFileName ="UDN_S/".$decode["channel"]["item"][$i]["title"].".json";
-        //以讀寫方式打寫指定檔案，如果檔案不存則建立
-        if (($TxtRes = fopen($TxtFileName, "w ")) === false) {
-            echo ("建立可寫檔案：" . $TxtFileName . "失敗");
-            exit();
-        }
-        echo ("建立可寫檔案" . $TxtFileName . "成功！</br>");
+        // $TxtFileName ="UDN_S/".$decode["channel"]["item"][$i]["title"].".json";
+        
+        if(isset($res_ar["content"])){
+            $TxtFileName ="UDN_S/".$i.".json";
 
-        //要 寫進檔案的內容
-        if (!fwrite($TxtRes, $res)) { 
-            //將資訊寫入檔案
-            echo ("嘗試向檔案" . $TxtFileName . "寫入" . $StrConents . "失敗！");
-            fclose($TxtRes);
-            exit();
+            //以讀寫方式打寫指定檔案，如果檔案不存則建立
+            if (($TxtRes = fopen($TxtFileName, "w ")) === false) {
+                echo ("建立可寫檔案：" . $TxtFileName . "失敗");
+                exit();
+            }
+            echo ("建立可寫檔案" . $TxtFileName . "成功！</br>");
+    
+            //要 寫進檔案的內容
+            if (!fwrite($TxtRes, $res_js)) { 
+                //將資訊寫入檔案
+                echo ("嘗試向檔案" . $TxtFileName . "寫入" . $StrConents . "失敗！");
+                fclose($TxtRes);
+                exit();
+            }
+            echo ("嘗試向檔案" . $TxtFileName . "寫入" . $StrConents . "成功！");
+            fclose($TxtRes); //關閉指標
         }
-        echo ("嘗試向檔案" . $TxtFileName . "寫入" . $StrConents . "成功！");
-        fclose($TxtRes); //關閉指標
+
     }
 }
 
--- read_UDN.php
+++ read_UDN.php
@@ -73,6 +73,11 @@
             background-image: url("img/內頁/外框.png");
             background-size: 100% 100%;
         }
+        .CompanyProfile{
+            color:white;
+            margin-top:2vmin;
+            font-size: 3vmin;
+        }
     </style>
     <script src="./general/menuicon.js"></script>
 
@@ -109,13 +114,14 @@
 
         <? include './general/start.php' ?>
 
-
+        <!-- 按鈕標頭。 -->
         <div class="row" style="text-align:center;margin-top:25px">
             <div class="col">
                 <h4 style="color:white;font-size: 6vmin;" id="choose_art">選擇有興趣的文章</h4>
             </div>
         </div>
 
+        <!-- 爬蟲文章按鈕 -->
         <div class="card" style="height: 70vmin;overflow-y: scroll;background-color:rgba(1,1,1,0);border: 0px white solid;margin-top: 20px;">
 
             <div class="card-body" style="background-color:rgba(1,1,1,0);">
@@ -123,13 +129,19 @@
                     <?php
                         $json_file = 'UDN_S/*.json';
                         $file_ar = glob($json_file);
+                        // var_dump($file_ar);
+                        
                         foreach($file_ar as $val){
+                            $file = file_get_contents($val);
+                            // var_dump(json_decode($file));
+                            $file_obj = json_decode($file);
+                            $title = $file_obj->title;
                             $url_decode = urldecode($val);
                             $url_s1 = substr_replace($url_decode,"",0,6);
                             $url = substr_replace($url_s1,"",strpos($url_s1,".json"),5);
                             echo "<div class='btn_title'>
-                            <div class='udnbtn' style='height:11vmin;overflow: hidden;color:white;line-height:12vmin;margin-left: 18vmin;font-size: 4vmin;'>"
-                            .$url.
+                            <div class='udnbtn' style='height:11vmin;overflow: hidden;color:white;line-height:12vmin;margin-left: 18vmin;font-size: 4vmin;' file_path='".$url."'>"
+                            .$title.
                             "</div>
                             </div>";
                         }
@@ -138,15 +150,15 @@
                 </ul>
             </div>
         </div>
-
+        <!-- 內文瀏覽標頭 -->
         <div class="row" style="text-align:center;margin-top:25px">
             <div class="col">
                 <h4 style="color:white;font-size: 6vmin;" id="art_content">內文瀏覽</h4>
             </div>
         </div>
 
-
-        <div class="card" style="height: 80vmin;overflow-y: scroll;background-color:rgba(1,1,1,0);border: 0px;padding-right: 5vmin;padding-left: 5vmin;padding-top: 2vmin;">
+        <!-- 文章內文顯示區 -->
+        <div class="card" style="height: 80vmin;overflow-y: scroll;background-color:rgba(1,1,1,0);border: 0px;padding-right: 5vmin;padding-left: 5vmin;padding-top: 2vmin;margin-top: 5vmin;">
 
             <div class="card-body" style="background-color:rgba(1,1,1,0);border: 1px #FFF solid;">
                 <ul class="list-group list-group-flush" id="content" style="color:white">
@@ -155,53 +167,96 @@
             </div>
         </div>
 
-
-        <div class="row" style="text-align:center;margin-top: 6vmin;">
+        <!-- 金幣按鈕_點擊觀看語意分析 -->
+        <div class="row" style="text-align:center;margin-top: 10vmin;">
             <div class="col">
                 <img id="gold_an" src="./img/內頁/BUTTON-1.png" class="img-fluid" style="width: 45vmin;">
             </div>
         </div>
-
-
-        <!-- <div class="card" style="height: 300px;overflow-y: scroll; background-color:rgba(1,1,1,0);">
-
-            <div class="card-body" style="background-color:rgba(1,1,1,0);">
-                <ul class="list-group list-group-flush" id="keyword" style="color:white">
-
-                </ul>
-            </div>
-        </div> -->
-        <div class="row" style="margin-top: 20px;padding-right:8vmin;padding-left:8vmin;display:none" id="anablock">
-            <div class="col" style="height:60vmin;color:white" id="keyword">
-                你好,嗨吃屎吧,我是誰
+        <!-- 關鍵字顯示區 -->
+        <div class="row" style="margin-top: 20px;padding-right:8vmin;padding-left:8vmin;height: 40vmin;" id="anablock">
+            <div class="col" style="height:50vmin;color:white;display:none" id="keyword">
             </div>
         </div>
-        <div class="row" style="text-align:center">
+        <!-- 寶藏相 -->
+        <div class="row" style="text-align:center;    margin-top: 10vmin;">
             <div class="col">
                 <img id="ana_btn" src="./img/內頁/寶藏-關.png" style="width: 40vmin;" class="img-fluid">
             </div>
         </div>
+        <!-- 語意分析 商績變業績 -->
+        <div class="row" style="text-align:center">
+            <div class="col">
+                <p style="color:white;font-size:2vmin">語意分析快速掌握消費者需求 讓商機變業績</p>
+            </div>
+        </div>
+        <!-- 隨身影音 -->
+        <div class="row" style=" margin-top: 8vmin;">
+            <div class="col" style="text-align:center">
+                <ins class="clickforceads" style="display:inline-block;width:301px;height:251px;" data-ad-zone="8660"></ins>
+                <script async type="text/javascript" src="//cdn.doublemax.net/js/init.js"></script>
+            </div>
+        </div>
 
-
-
+        <!-- 同場加映 -->
+        <div class="row" style="text-align:center;margin-top: 5vmin;">
+            <div class="col">
+                <h4 style="color:white">同場加映</h4>
+                <h4 style="color:white">電信大數據應用</h4>
+            </div>
+        </div>
+        <!-- 比利哥YouTube -->
+        <div class="row" style="text-align:center;margin-top: 5vmin;">
+            <div class="col">
+                <iframe src="https://www.youtube.com/embed/CnT1IniYwIY?rel=0&autoplay=1" frameborder="0" allowfullscreen allow="autoplay"></iframe>
+            </div>
+        </div>
+        <!-- 公司簡介 -->
+        <div class="row" style="text-align:center;margin-top: 5vmin;">
+            <div class="col">
+                <h4 style="color:white">公司簡介</h4>
+                <p class="CompanyProfile">域動行銷致力發展數位媒體聯播網絡，</p>
+                <p class="CompanyProfile">專注於數位網路動態發展、</p>
+                <p class="CompanyProfile">洞察媒體趨勢與消費者行為興趣分析。</p>
+                <p class="CompanyProfile">擁有國際化規格之DSP/SSP/DMP廣告系統，</p>
+                <p class="CompanyProfile">透過AI優化引擎Holmes 數位廣告先知核心，</p>
+                <p class="CompanyProfile">全面提升廣告自動優化技術。</p>
+                <p class="CompanyProfile">提供廣告主數位媒體最佳整合行銷方案</p>
+            </div>
+        </div>
+        <!-- 公司標語 -->
+        <div class="row" style="margin-top:10vmin;">
+          <div class="col" style="padding: 0px;">
+            <img src="./img/內頁/KPI要達標 就找域動行銷.png" alt="" class="img-fluid">
+          </div>
+        </div>
+        <div class="row" style="margin-top:-1px;">
+          <div class="col" style="padding: 0px;">
+            <img src="./img/內頁/電話.png" alt="" class="img-fluid">
+          </div>
+          <div class="col" style="padding: 0px;">
+            <img src="./img/內頁/MAIL.png" alt="" class="img-fluid">
+          </div>
+        </div>
     </div>
     <script>
         $(function () {
-            $("#ana_btn").click(function(){
 
-                $("#anablock").css('display','block');
-            })
             $(".udnbtn").click(function () {
+                $("#ana_btn").attr("src", "./img/內頁/寶藏-關.png");
+
                 $([document.documentElement, document.body]).animate({
                     scrollTop: $("#art_content").offset().top
                 }, 1000);
-                var url = this.innerText;
+                var url = $(this).attr("file_path");
+                console.log(url);
                 // $.getJSON('https://events.clickforce.com.tw/digiasia2018/' + url, function (data) {
 
                 $.getJSON('http://localhost:8888/digiasia2018/UDN_S/' + url + '.json', function (data) {
-                    $("#content").text = "";
+                    $("#content").text("");
                     $("#content").text(data.content);
-                    $("#keyword").text = "";
+                    $("#keyword").css('display', 'none');
+                    $("#keyword").text("");
                     $("#keyword").text(data.keywords);
                     opacityset('content', 0);
                     opacityset('keyword', 0);
@@ -230,6 +285,19 @@
             });
             $("#gold_an").on('touchend', function () {
                 $("#gold_an").attr('src', './img/內頁/BUTTON-1.png');
+            })
+            $("#gold_an").click(function () {
+                $([document.documentElement, document.body]).animate({
+                    scrollTop: $("#gold_an").offset().top,
+                    opacity: 1
+                }, 1000);
+            })
+            //點了寶藏香後
+            $("#ana_btn").click(function () {
+                $("#ana_btn").attr("src", "./img/內頁/寶藏-開.png");
+
+                $("#keyword").css('display', 'block');
+                opacityset('keyword', 0);
             })
 
         })

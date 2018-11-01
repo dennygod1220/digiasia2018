7b063d40d2c0ca0ff0fa96efc7556aa76707b1df
Dennygod <dennygod1220@gmail.com>
2018-11-01 22:03:37 +0800

    1101_3

--- read_UDN.php
+++ read_UDN.php
@@ -312,7 +312,7 @@
 
 
                     $("#content").text("");
-                    $("#content").append('<iframe src="' + url +
+                    $("#content").append('<iframe src="https://events.clickforce.com.tw/digiasia2018/test.php?url=' + url +
                         '" style="width:100%;height:100%" frameBorder="0">');
 
                     $("#keyword").css('display', 'none');

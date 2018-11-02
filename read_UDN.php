a2fd19ba95e447b4aa219fddc366f260939d6f83
Dennygod <dennygod1220@gmail.com>
2018-11-02 15:20:13 +0800

    1102_27

--- read_UDN.php
+++ read_UDN.php
@@ -336,7 +336,7 @@
 
                     if (navigator.userAgent.match(/android/i)) {
                         //如果是Android的話
-                        $("#content").append('<iframe id="content_if" src="' + url +'" style="width: 1px; min-width: 100%;*width: 100%;" frameBorder="0">');
+                        $("#content").append('<iframe id="content_if" src="' + url +'" style="width:100%;height:100%" frameBorder="0">');
                             var if_h = $("#content").css('height'); 
                             if_h = parseInt(if_h.replace("px", ""));
                             var if_w = $("#content").css('width'); 

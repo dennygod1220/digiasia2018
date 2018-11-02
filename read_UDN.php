81cc541fa3e36cdcdc6887b037e6948a3b10d568
Dennygod <dennygod1220@gmail.com>
2018-11-02 14:48:14 +0800

    1102_18

--- read_UDN.php
+++ read_UDN.php
@@ -322,11 +322,11 @@
                     console.log( "height " + $("#content").css('height'));
                     console.log( "width " + $("#content").css('width'));
                     var if_h = $("#content").css('height');
-                    if_h = if_h.replace("px","");
+                    if_h = parseInt(if_h.replace("px",""))-20;
                     var if_w = $("#content").css('width');
                     if_w = if_w.replace("px","");
                     $("#content_if").attr("width",if_w-10);
-                    $("#content_if").attr("height",if_h-20);
+                    $("#content_if").attr("height",if_h);
 
 
                     $("#keyword").css('display', 'none');
